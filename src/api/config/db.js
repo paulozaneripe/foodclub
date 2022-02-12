f1ecbadcaf26d5bca0ae0318ea79c099343b27f4
paulozaneripe <paulozaneripe@hotmail.com>
2022-02-12 14:27:19 -0300

    Manutenção banco postgres na Heroku

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -13,7 +13,10 @@ const productionDb = {
     password: 'ea3589627aafc945a172920b9ca830fc1c5b81af756a5407cf81cbfdeac1d851',
     host: 'ec2-54-224-64-114.compute-1.amazonaws.com',
     port: 5432,
-    database: 'd5fsobskme6jkb'
+    database: 'd5fsobskme6jkb',
+    ssl: {
+        rejectUnauthorized: false,
+    }
 };
 
 export default new Pool(
